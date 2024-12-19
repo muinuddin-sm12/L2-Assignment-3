b046895682167bff934decb2d32173512e45d964
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2024-12-19 22:06:18 +0400

    add: notFound middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express, { Application } from 'express';
 import router from './app/router';
+import notFound from './app/middlewares/notFound';
 
 const app: Application = express();
 
@@ -14,5 +15,10 @@ app.use('/api', router)
 app.get('/', (req, res) => {
     res.send('Welcome to L-2 Assignmet-3')
 })
+// global error handle 
+
+
+// not found 
+app.use(notFound);
 
 export default app;
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,18 +1,15 @@
 import { Request, Response } from 'express';
 import { UserServices } from './user.service';
+import catchAsync from '../../utils/catchAsync';
 
-const createUser = async (req: Request, res: Response) => {
-  try {
-    const result = await UserServices.createUserIntoDB(req.body);
-    res.status(200).json({
-      success: true,
-      message: 'User created successfully',
-      data: result,
-    });
-  } catch (err) {
-    console.log(err);
-  }
-};
+const createUser = catchAsync(async (req: Request, res: Response) => {
+  const result = await UserServices.createUserIntoDB(req.body);
+  res.status(200).json({
+    success: true,
+    message: 'User created successfully',
+    data: result,
+  });
+});
 
 export const UserController = {
   createUser,
