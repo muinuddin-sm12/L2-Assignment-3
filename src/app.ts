386234c313817e406224370cdcc1f66c52c9a6f7
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2024-12-19 14:59:14 +0400

    add: user route and user controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express, { Application } from 'express';
+import router from './app/router';
 
 const app: Application = express();
 
@@ -8,6 +9,7 @@ app.use(express.json())
 app.use(cors())
 
 // application routes
+app.use('/api', router)
 
 app.get('/', (req, res) => {
     res.send('Welcome to L-2 Assignmet-3')
---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+const createUser = async (req:Request, res:Response) => {
+    try{
+        const result = await 
+    }catch(err){
+
+    }
+}
+
+export const UserController = {
+    createUser,
+}
