571220332361686f83b50b9cf08d3e99ae543991
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2024-12-19 17:50:39 +0400

    add: catchAsync utils

---
+++ src/app/middlewares/validateRequest.ts
@@ -0,0 +1,5 @@
+import { AnyZodObject } from "zod";
+
+const validateRequest = (schema:AnyZodObject) => {
+    return 
+}
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import { UserController } from './user.controller';
+import { UserValidation } from './user.validation';
 const router = express.Router();
 
-router.post('/register', UserController.createUser);
+router.post('/register', UserValidation.userValidationSchema, UserController.createUser);
 
 export const UserRoutes = router;
