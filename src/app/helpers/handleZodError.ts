7b9d645f9ea7548e67cd38c58f90cec40518d220
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2024-12-19 22:18:10 +0400

    add: zod error handle helper

---
+++ src/app/helpers/handleZodError.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Response } from "express";
+
+export const handleZodError = (err: any, res: Response) => {
+    const issues = err.issues.map((item: any) => {
+        return {
+            path: item.path.join('>'),
+            message: item.message
+        }
+    })
+    res.status(400).json({
+        success: false,
+        message: err.message,
+        issues,
+        error: err
+    })
+}
