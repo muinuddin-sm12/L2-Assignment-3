fe8e773b155fab703d3797b4a6db4923d89e2940
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2024-12-20 04:05:27 +0400

    add: auth route file in auth module

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { UserRoutes } from '../modules/user/user.route';
 import { BlogRoutes } from '../modules/blog/blog.route';
+import { AuthRoutes } from '../modules/auth/auth.route';
 
 const router = Router();
 
@@ -12,6 +13,10 @@ const moduleRoutes = [
   {
     path: '/blogs',
     route: BlogRoutes,
+  },
+  {
+    path: '/auth',
+    route: AuthRoutes,
   }
 ];
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
